b553c3efb28a639bd71436940b3ebdb375da5d99
Kadir Dagli <mehmetkadirdagli27@gmail.com>
2024-10-30 00:48:38 +0300

    Add project files.

---
+++ MyAspNetCore/Controllers/OrnekController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAspNetCore.Controllers
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    
+    }
+    public class OrnekController : Controller
+    {
+        public IActionResult Index()
+        {
+            var productlist = new List<Product>()
+            {
+                new(){Id=1 , Name="kalem"},
+                new(){Id=2 , Name="Silgi"},
+                new(){Id=3 , Name="Cüzdanlık"}
+
+            };
+
+            //ViewBag.name = "Ahmet";
+            
+            ///TempData["surname"] = "yıldız";
+            return View(productlist);
+        }
+
+        public IActionResult ContentResult()
+        {
+            return Content("Content result");
+        }
+
+        public IActionResult Index2()
+        {
+           // return RedirectToAction("Index","Ornek");
+           return View();
+        }
+    }
+}
